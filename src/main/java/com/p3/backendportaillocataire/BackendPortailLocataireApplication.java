1e382ee57c936c5b0d60c006304981cba52316d4
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-04-16 09:23:10 +0200

    :tada: Initial commit

---
+++ src/main/java/com/p3/backendportaillocataire/BackendPortailLocataireApplication.java
@@ -0,0 +1,13 @@
+package com.p3.backendportaillocataire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPortailLocataireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPortailLocataireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p3/backendportaillocataire/BackendPortailLocataireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p3.backendportaillocataire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPortailLocataireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
